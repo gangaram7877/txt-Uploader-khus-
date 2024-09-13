d69887c38b5097dcbf2ac42e3356c04995e32585
gangaram7877 <147220752+gangaram7877@users.noreply.github.com>
2024-09-13 21:27:51 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "26466863"
 api_hash = "43eb777ae460ede6129c6549e5ba7817"
-bot_token = "7533743552:AAH_4mg80Ct6unaD5KNdHfSDITZ1jp_PU90"
+bot_token = "7120424126:AAGmpbyT-bURbVQfFbpUMTD1F_6p7W1oa2o"
